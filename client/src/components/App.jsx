0cba89686c2f46ff377dd9c1d89e83e6f442a28b
Parteek S. Sandhu <ParteekSSandhu@MacBook-Pro-5.local>
2019-11-18 14:38:36 -0800

    Get apod information from nasa api

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,17 +1,107 @@
 import React from 'react';
+import Axios from 'axios';
+import nasaApi from '../../../NASA_KEY.js';
+import Apod from './Apod.jsx';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      // state goes here
+      apod: {},
+      issPosition: {},
+      curiosity: {},
+      spirit: {},
+      opportunity: {},
+      people: []
     };
+    this.getApod = this.getApod.bind(this);
+    this.getIssPosition = this.getIssPosition.bind(this);
+    this.getCuriosityInfo = this.getCuriosityInfo.bind(this);
+    this.getSpiritInfo = this.getSpiritInfo.bind(this);
+    this.getOpportunityInfo = this.getOpportunityInfo.bind(this);
+    this.getPeople = this.getPeople.bind(this);
+  }
+
+  // get nasa astronomy image of the day
+  getApod() {
+    Axios.get(`https://api.nasa.gov/planetary/apod?api_key=${nasaApi}`)
+      .then((res) => {
+        // console.log('APOD call: ', res.data);
+        this.setState({
+          apod: res.data
+        });
+      });
+  }
+
+  getIssPosition() {
+    Axios.get('http://api.open-notify.org/iss-now.json')
+      .then((res) => {
+        // console.log('ISS call: ', res.data);
+        this.setState({
+          issPosition: res.data.iss_position
+        });
+      });
+  }
+
+  getCuriosityInfo() {
+    Axios.get(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity?api_key=${nasaApi}`)
+      .then((res) => {
+        // console.log('CURIOSITY call: ', res.data.rover);
+        this.setState({
+          curiosity: res.data.rover
+        });
+      });
+  }
+
+  getSpiritInfo() {
+    Axios.get(`https://api.nasa.gov/mars-photos/api/v1/rovers/spirit?api_key=${nasaApi}`)
+      .then((res) => {
+        // console.log('SPIRIT call: ', res.data.rover);
+        this.setState({
+          spirit: res.data.rover
+        });
+      });
+  }
+
+  getOpportunityInfo() {
+    Axios.get(`https://api.nasa.gov/mars-photos/api/v1/rovers/opportunity?api_key=${nasaApi}`)
+      .then((res) => {
+        // console.log('OPPORTUNITY call: ', res.data.rover);
+        this.setState({
+          opportunity: res.data.rover
+        });
+      });
+  }
+
+  getPeople() {
+    Axios.get('http://api.open-notify.org/astros.json')
+      .then((res) => {
+        // console.log('PEOPLE call: ', res.data.people);
+        this.setState({
+          people: res.data.people
+        });
+      });
+  }
+
+  componentDidMount() {
+    this.getApod();
+    // this.getIssPosition();
+    // this.getCuriosityInfo();
+    // this.getSpiritInfo();
+    // this.getOpportunityInfo();
+    // this.getPeople();
   }
 
   render() {
     return (
       <div>
-        <p>Time: {this.props.time}</p>
+        {console.log('APOD: ', this.state.apod)}
+        {/* {console.log('Iss Position: ', this.state.issPosition)} */}
+        {/* {console.log('CURIOSITY: ', this.state.curiosity)} */}
+        {/* {console.log('SPIRIT: ', this.state.spirit)} */}
+        {/* {console.log('OPPORTUNITY: ', this.state.opportunity)} */}
+        {/* {console.log('PEOPLE: ', this.state.people)} */}
+        <Apod apod={this.state.apod} />
       </div>
     );
   }
--- server/router.js
+++ server/router.js
@@ -8,6 +8,11 @@ var timeLog = (req, res) => {
   res.status(200).send(time);
 };
 
+// callback to get nasa astronomy picture of the day
+// var apod = (req, res) => {
+//   res.send(200).status(data);
+// };
+
 // test get req to api route
 router.get('/', (req, res) => {
   res.status(200).send('Api is working!');
